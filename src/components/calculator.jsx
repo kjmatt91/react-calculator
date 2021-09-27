430c0fc2117158e04ca59a4f32774ed3070075d2
kjmatt91 <kjmatt91@gmail.com>
2021-09-26 23:12:54 -0400

    started setup of switch case in calculator component

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Calculate = () => {
+
+    const [calculation, setCalculation] = useState('')
+    const sum = () => {
+        const field1 = document.getElementById('num1').value
+        const field2 = document.getElementById('num2').value
+        const operator = document.getElementById('operator').value
+
+        if(!isNaN(total)) {
+            switch(operator) {
+                case '+':
+                    setCalculation(field1) + (field2)
+                    break
+                case '-':
+                    setCalculation(field1) - (field2)
+                    break
+                case '*':
+                    setCalculation(field1) * (field2)
+                    break
+                case '/':
+                    setCalculation(field1) / (field2)
+                    break
+                default:
+                    setCalculation(0)
+            }
+        } else {
+            setCalculation('Please Enter Valid Numbers')
+        }
+    }
     return (
         <div className="page">
             
